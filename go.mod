eb5a6cf15ed729078aacb2b3781d07314c389f65
pasDamola <oyindavid18@gmail.com>
2023-09-24 23:21:20 +0100

    structuring the lgs applications

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.19
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "github.com/pasDamola/go-microservices/app"
+import (
+	"github.com/pasDamola/go-microservices/app"
+	"github.com/pasDamola/go-microservices/logger"
+)
 
 
 func main() {
 
+	logger.Info("Starting the application...")
 	app.Start()
 	
 }
